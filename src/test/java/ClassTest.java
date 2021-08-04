8f5a85566c7904e39d69bf7691fcef9f781da4f2
Ivan Rubinov <lrubinov@yandex.ru>
2021-08-04 14:36:36 +0300

    No errors

---
+++ src/test/java/ClassTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Calendar;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ClassTest {
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        String timeStamp = new SimpleDateFormat("dd.MM.yyyy").format(Calendar.getInstance().getTime());
+        //SelenideElement form = $("[data-test-id=callback-form]");
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(timeStamp);
+        $("[data-test-id=name] input").setValue("Василий Пупкин");
+        $("[data-test-id=phone] input").setValue("+79160123456");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+       $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(20));
+    }
+}
