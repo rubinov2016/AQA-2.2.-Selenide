3c7dd5799d198e03b11732f74ce534ab4a6edd66
Ivan Rubinov <lrubinov@yandex.ru>
2021-09-05 09:45:32 +0300

    Third fix

--- src/test/java/ClassTest.java
+++ src/test/java/ClassTest.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -15,12 +17,13 @@ public class ClassTest {
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999");
-        Date currentDate = new Date();
-        Calendar c = Calendar.getInstance();
-        c.setTime(currentDate);
-        c.add(Calendar.DATE, 4);
-        Date currentDatePlusFour = c.getTime();
-        String timeStamp = new SimpleDateFormat("dd.MM.yyyy").format(currentDatePlusFour);
+//        Date currentDate = new Date();
+//        Calendar c = Calendar.getInstance();
+//        c.setTime(currentDate);
+//        c.add(Calendar.DATE, 4);
+//        Date currentDatePlusFour = c.getTime();
+//        String timeStamp = new SimpleDateFormat("dd.MM.yyyy").format(currentDatePlusFour);
+        String timeStamp = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
         $("[data-test-id=city] input").setValue("Москва");
 
@@ -30,7 +33,8 @@ public class ClassTest {
         $("[data-test-id=phone] input").setValue("+79160123456");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-       $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id=success-notification]").find(timeStamp);
+      // $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + timeStamp));
+        //$("[data-test-id=success-notification]").find(timeStamp);
     }
 }
